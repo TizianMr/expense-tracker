a819f045e00481410780c23ad44f18fabd0ff5e9
TizianMr <tizianmr@gmail.com>
2025-03-23 16:01:15 +0100

    feat: implemented confirm deletion dialog

---
+++ app/components/confirmation-dialog.tsx
@@ -0,0 +1,68 @@
+import { Button, Text } from '@chakra-ui/react';
+
+import {
+  DialogActionTrigger,
+  DialogBackdrop,
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+} from './ui/dialog';
+
+type Props = {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  isSubmitting: boolean;
+  secondaryActionText?: string;
+  primaryActionText?: string;
+  onClose: () => void;
+  onSubmit: () => void;
+};
+
+export const ConfirmationDialog = ({
+  title,
+  description,
+  isOpen,
+  isSubmitting,
+  primaryActionText = 'Delete',
+  secondaryActionText = 'Cancel',
+  onClose,
+  onSubmit,
+}: Props) => {
+  return (
+    <DialogRoot
+      open={isOpen}
+      onOpenChange={onClose}>
+      <DialogBackdrop />
+      <DialogContent>
+        <DialogCloseTrigger />
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+        </DialogHeader>
+        <DialogBody>
+          <Text>{description}</Text>
+        </DialogBody>
+        <DialogFooter>
+          <DialogActionTrigger asChild>
+            <Button variant='outline'>{secondaryActionText}</Button>
+          </DialogActionTrigger>
+          <Button
+            colorPalette='red'
+            loading={isSubmitting}
+            form='expenseForm'
+            onClick={onSubmit}
+            type='submit'>
+            {primaryActionText}
+          </Button>
+        </DialogFooter>
+        <DialogCloseTrigger />
+      </DialogContent>
+    </DialogRoot>
+  );
+};
+
+export default ConfirmationDialog;
