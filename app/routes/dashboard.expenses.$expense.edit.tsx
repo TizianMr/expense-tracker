4d282af4d6a26a3a2427dc0f17c915e822e70448
TizianMr <tizianmr@gmail.com>
2025-04-06 12:26:47 +0200

    refactor: merged edit and create expense forms into one component

--- app/routes/dashboard.expenses.$expense.edit.tsx
+++ app/routes/dashboard.expenses.$expense.edit.tsx
@@ -1,24 +1,11 @@
-import { createListCollection, Input, ListCollection, Stack } from '@chakra-ui/react';
-import { Budget, Category, Expense } from '@prisma/client';
+import { Alert } from '@chakra-ui/react';
+import { Category } from '@prisma/client';
 import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
 import { useLoaderData, useOutletContext } from '@remix-run/react';
-import { useEffect, useState } from 'react';
-import { FaEuroSign } from 'react-icons/fa';
 
 import { FormErrors } from './dashboard.expenses';
-import { Field } from '../components/ui/field';
-import { InputGroup } from '../components/ui/input-group';
-import { NumberInputField, NumberInputRoot } from '../components/ui/number-input';
-import {
-  SelectContent,
-  SelectItem,
-  SelectLabel,
-  SelectRoot,
-  SelectTrigger,
-  SelectValueText,
-} from '../components/ui/select';
 import { fetchExpenseById, updateExpense, UpdateExpense } from '../db/expense.server';
-import { EXPENSE_CATEGORIES } from '../utils/constants';
+import ExpenseForm from '~/components/expense-form';
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   const expenseId = params.expense as string;
@@ -67,88 +54,21 @@ const EditExpenseDialog = () => {
   //   }
   // }, [budgets]);
 
+  if (!expense) {
+    return (
+      <Alert.Root status='error'>
+        <Alert.Indicator />
+        <Alert.Title>The expense you are trying to edit could not be found.</Alert.Title>
+      </Alert.Root>
+    );
+  }
+
   return (
-    <Stack gap='4'>
-      <Field
-        required
-        errorText={errors.title}
-        invalid={!!errors.title}
-        label='Title'>
-        <Input
-          defaultValue={expense?.title ?? ''}
-          name='title'
-          placeholder='test'
-        />
-      </Field>
-      <Field
-        required
-        errorText={errors.amount}
-        invalid={!!errors.amount}
-        label='Amount'>
-        <NumberInputRoot
-          allowMouseWheel
-          defaultValue={expense?.amount.toString() ?? '0'}
-          formatOptions={{
-            maximumFractionDigits: 2,
-            minimumFractionDigits: 2,
-          }}
-          locale='de-DE'
-          name='amount'
-          width='100%'>
-          <InputGroup
-            startElement={<FaEuroSign />}
-            width={'100%'}>
-            <NumberInputField pattern='\d{1,3}(.\d{3})*(,\d{2})?' />
-          </InputGroup>
-        </NumberInputRoot>
-      </Field>
-      <Field
-        required
-        errorText={errors.date}
-        invalid={!!errors.date}
-        label='Date'>
-        <Input
-          defaultValue={expense?.expenseDate.toISOString().split('T')[0] ?? ''}
-          name='date'
-          type='date'
-        />
-      </Field>
-      <SelectRoot
-        collection={EXPENSE_CATEGORIES}
-        defaultValue={[expense?.category ?? '']}
-        name='category'>
-        <SelectLabel>Category</SelectLabel>
-        <SelectTrigger>
-          <SelectValueText placeholder='Select category' />
-        </SelectTrigger>
-        <SelectContent portalRef={contentRef}>
-          {EXPENSE_CATEGORIES.items.map(item => (
-            <SelectItem
-              item={item}
-              key={item.value}>
-              {item.label}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </SelectRoot>
-      {/* <SelectRoot
-        collection={budgetCollection}
-        name='budget'>
-        <SelectLabel>Budget</SelectLabel>
-        <SelectTrigger>
-          <SelectValueText placeholder='Select budget' />
-        </SelectTrigger>
-        <SelectContent portalRef={contentRef}>
-          {budgetCollection.items.map(item => (
-            <SelectItem
-              item={item}
-              key={item.value}>
-              {item.label}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </SelectRoot> */}
-    </Stack>
+    <ExpenseForm
+      contentRef={contentRef}
+      errors={errors}
+      expense={expense}
+    />
   );
 };
 
--- app/routes/dashboard.expenses.create.tsx
+++ app/routes/dashboard.expenses.create.tsx
@@ -1,23 +1,9 @@
-import { createListCollection, Input, ListCollection, Stack } from '@chakra-ui/react';
-import { Budget, Category, Expense } from '@prisma/client';
+import { Category } from '@prisma/client';
 import { ActionFunctionArgs } from '@remix-run/node';
 import { useOutletContext } from '@remix-run/react';
-import { useEffect, useState } from 'react';
-import { FaEuroSign } from 'react-icons/fa';
 
 import { FormErrors } from './dashboard.expenses';
-import { Field } from '../components/ui/field';
-import { InputGroup } from '../components/ui/input-group';
-import { NumberInputField, NumberInputRoot } from '../components/ui/number-input';
-import {
-  SelectContent,
-  SelectItem,
-  SelectLabel,
-  SelectRoot,
-  SelectTrigger,
-  SelectValueText,
-} from '../components/ui/select';
-import { EXPENSE_CATEGORIES } from '../utils/constants';
+import ExpenseForm from '~/components/expense-form';
 import { createExpense, CreateExpense } from '~/db/expense.server';
 
 export const action = async ({ request }: ActionFunctionArgs) => {
@@ -58,84 +44,10 @@ const CreateExpenseDialog = () => {
   // }, [budgets]);
 
   return (
-    <Stack gap='4'>
-      <Field
-        required
-        errorText={errors.title}
-        invalid={!!errors.title}
-        label='Title'>
-        <Input
-          name='title'
-          placeholder='Groceries'
-        />
-      </Field>
-      <Field
-        required
-        errorText={errors.amount}
-        invalid={!!errors.amount}
-        label='Amount'>
-        <NumberInputRoot
-          allowMouseWheel
-          defaultValue='0'
-          formatOptions={{
-            maximumFractionDigits: 2,
-            minimumFractionDigits: 2,
-          }}
-          locale='de-DE'
-          name='amount'
-          width='100%'>
-          <InputGroup
-            startElement={<FaEuroSign />}
-            width={'100%'}>
-            <NumberInputField pattern='\d{1,3}(.\d{3})*(,\d{2})?' />
-          </InputGroup>
-        </NumberInputRoot>
-      </Field>
-      <Field
-        required
-        errorText={errors.date}
-        invalid={!!errors.date}
-        label='Date'>
-        <Input
-          name='date'
-          type='date'
-        />
-      </Field>
-      <SelectRoot
-        collection={EXPENSE_CATEGORIES}
-        name='category'>
-        <SelectLabel>Category</SelectLabel>
-        <SelectTrigger>
-          <SelectValueText placeholder='Select category' />
-        </SelectTrigger>
-        <SelectContent portalRef={contentRef}>
-          {EXPENSE_CATEGORIES.items.map(item => (
-            <SelectItem
-              item={item}
-              key={item.value}>
-              {item.label}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </SelectRoot>
-      {/* <SelectRoot
-        collection={budgetCollection}
-        name='budget'>
-        <SelectLabel>Budget</SelectLabel>
-        <SelectTrigger>
-          <SelectValueText placeholder='Select budget' />
-        </SelectTrigger>
-        <SelectContent portalRef={contentRef}>
-          {budgetCollection.items.map(item => (
-            <SelectItem
-              item={item}
-              key={item.value}>
-              {item.label}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </SelectRoot> */}
-    </Stack>
+    <ExpenseForm
+      contentRef={contentRef}
+      errors={errors}
+    />
   );
 };
 
