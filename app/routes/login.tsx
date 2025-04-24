c16ef3e827a1c8dd2e5b34927438d698596a981c
TizianMr <tizianmr@gmail.com>
2025-04-24 23:34:11 +0200

    feat: login page

---
+++ app/routes/login.tsx
@@ -0,0 +1,70 @@
+import { NavLink } from '@remix-run/react';
+import { TextInput } from '@tremor/react';
+
+const LoginPage = () => {
+  return (
+    <>
+      <div className='flex min-h-screen flex-1 flex-col justify-center px-4 py-10 lg:px-6'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+          <img
+            alt='Expense tracker logo'
+            className='mx-auto'
+            src='logo.png'
+            width='150rem'
+          />
+          <h3 className='text-center text-tremor-title font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong'>
+            Login
+          </h3>
+          <form
+            action='#'
+            className='mt-6 space-y-4'
+            method='post'>
+            <div>
+              <label
+                className='text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong'
+                htmlFor='email'>
+                Email
+              </label>
+              <TextInput
+                autoComplete='email'
+                className='mt-2'
+                id='email'
+                name='email'
+                placeholder='john@company.com'
+                type='email'
+              />
+            </div>
+            <div>
+              <label
+                className='text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong'
+                htmlFor='password'>
+                Password
+              </label>
+              <TextInput
+                autoComplete='password'
+                className='mt-2'
+                id='password'
+                name='password'
+                placeholder='password'
+                type='password'
+              />
+            </div>
+            <button
+              className='mt-4 w-full whitespace-nowrap rounded-tremor-default bg-tremor-brand py-2 text-center text-tremor-default font-medium text-tremor-brand-inverted shadow-tremor-input hover:bg-tremor-brand-emphasis dark:bg-dark-tremor-brand dark:text-dark-tremor-brand-inverted dark:shadow-dark-tremor-input dark:hover:bg-dark-tremor-brand-emphasis'
+              type='submit'>
+              Login
+            </button>
+          </form>
+          <p className='mt-4 text-tremor-label text-tremor-content dark:text-dark-tremor-content text-center'>
+            Don&apos;t have an account?{' '}
+            <span className='underline underline-offset-4'>
+              <NavLink to={'/signup'}>Sign up</NavLink>
+            </span>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
