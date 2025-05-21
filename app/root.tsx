bdb614c4525e4ffc9028557305abb75eb8e7fe78
TizianMr <tizianmr@gmail.com>
2025-05-21 15:43:17 +0200

    feat: basic i18n setup

--- app/root.tsx
+++ app/root.tsx
@@ -1,8 +1,23 @@
+import { LoaderFunctionArgs } from '@remix-run/node';
 import { Links, Meta, MetaFunction, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 import './tailwind.css';
 
 import { cx } from './utils/helpers';
+import i18next from './utils/i18n/i18next.server';
 import { NonFlashOfWrongThemeEls, ThemeProvider, useTheme } from './utils/theme-provider';
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const locale = await i18next.getLocale(request);
+  return { locale };
+}
+
+export const handle = {
+  // In the handle export, we can add a i18n key with namespaces our route
+  // will need to load. This key can be a single string or an array of strings.
+  // TIP: In most cases, you should set this to your defaultNS from your i18n config
+  // or if you did not set one, set it to the i18next default namespace "translation"
+  i18n: 'common',
+};
 
 export const meta: MetaFunction = () => {
   return [
