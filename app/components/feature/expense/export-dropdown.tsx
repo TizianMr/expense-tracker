a8299a5596cd63482bec65be33d3fb7710a35c81
TizianMr <tizianmr@gmail.com>
2025-09-14 19:50:07 +0200

    feat: handle export as csv

--- app/components/feature/expense/export-dropdown.tsx
+++ app/components/feature/expense/export-dropdown.tsx
@@ -4,7 +4,9 @@ import { Button } from '@tremor/react';
 import { useTranslation } from 'react-i18next';
 
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../../ui/dropdown';
+import { convertJSONToCSV } from '~/utils/helpers';
 
+// TODO: Toast message when nothing to export
 export const ExportDropdown = () => {
   const { t } = useTranslation();
 
@@ -40,27 +42,57 @@ export const ExportDropdown = () => {
     URL.revokeObjectURL(url);
   };
 
+  const handleExportAsCsv = async () => {
+    const res = await fetch('/api/export');
+    const expenses: Expense[] = await res.json();
+
+    const headers = ['title', 'amount', 'date', 'category'];
+
+    const csvData = convertJSONToCSV(
+      expenses.map(expense => ({
+        title: expense.title,
+        amount: expense.amount,
+        date: expense.createdAt,
+        category: expense.category,
+      })),
+      headers,
+    );
+
+    const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.setAttribute('download', 'expenses.csv');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
-    <>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            icon={RiFileDownloadLine}
-            variant='secondary'
-          />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent
-          align='end'
-          className='min-w-40'>
-          <DropdownMenuItem
-            onSelect={e => {
-              e.preventDefault();
-              handleExportAsJson();
-            }}>
-            {t('ExportDropdown.exportAsJson')}
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          icon={RiFileDownloadLine}
+          variant='secondary'
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align='end'
+        className='min-w-40'>
+        <DropdownMenuItem
+          onSelect={e => {
+            e.preventDefault();
+            handleExportAsJson();
+          }}>
+          {t('ExportDropdown.exportAsJson')}
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onSelect={e => {
+            e.preventDefault();
+            handleExportAsCsv();
+          }}>
+          {t('ExportDropdown.exportAsCsv')}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
--- app/utils/helpers.ts
+++ app/utils/helpers.ts
@@ -12,3 +12,16 @@ export function cx(...args: ClassValue[]) {
 export const getS3ObjectKey = (objUrl: string) => {
   return objUrl.split('/')[objUrl.split('/').length - 1];
 };
+
+// https://dev.to/cybercop/converting-json-to-csv-in-react-a-diy-approach-59lk
+export const convertJSONToCSV = (jsonData: Record<string, unknown>[], columnHeaders: string[]): string => {
+  const headers = columnHeaders.join(',') + '\n';
+
+  const rows = jsonData
+    .map(row => {
+      return columnHeaders.map(field => row[field] || '').join(',');
+    })
+    .join('\n');
+
+  return headers + rows;
+};
